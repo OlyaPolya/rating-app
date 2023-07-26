cf6c584e16089c8d47aefdbb636dbac613782305
Olya <polyanskaya3000@gmail.com>
2023-07-26 15:39:05 +0700

    feat: add basic structure

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import Head from 'next/head';
+import React from 'react';
+import { AppProps } from 'next/dist/shared/lib/router/router';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  return (
+    <>
+      <Head>
+        <title>MyTop - наш лучший топ</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
+
+export default MyApp;
+
+
